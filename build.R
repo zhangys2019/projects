8999056d928f7c43946e97be7d5d76a7fd62638b
John Coene <jcoenep@gmail.com>
2017-08-04 18:43:32 +0800

    about + book tweaks

--- build.R
+++ build.R
@@ -9,3 +9,4 @@ render_site("./packages/bibler") # bibler
 render_site("./packages/rodham") # rodham
 render_site("./packages/graphTweets") # graphTweets
 render_site("./htmlwidgets/echarts") # echarts
+render_site("./tour")
